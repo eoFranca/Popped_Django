e474f3bb1574b64fb50d449e1af992f009bc8862
eoFrancaa <erfranca.com@gmail.com>
2024-08-24 14:13:14 -0300

    Adiciona relacionamento de Produto com Categoria .

--- core/models/produto.py
+++ core/models/produto.py
@@ -1,10 +1,12 @@
 from django.db import models
+from .categoria import Categoria    
+
 
 class Produto (models.Model):
     nome = models.CharField(max_length=100,blank=False,null=True)
     descricao = models.CharField(max_length=100,blank=False,null=True)
     valor = models.DecimalField(max_digits=10, decimal_places=2)
-
+    categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, related_name="livros", null=True, blank=True)
 
     def __str__(self):
         return f'{self.nome} - {self.descricao} '
