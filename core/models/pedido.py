21be7afb0a02ba6c04456c45104193b4bf0b41c5
LeandroVish <leandrogustavosantos.info@gmail.com>
2024-10-04 09:21:29 -0300

    up

--- core/models/pedido.py
+++ core/models/pedido.py
@@ -8,6 +8,7 @@ class Pedido(models.Model):
     data_criacao = models.DateTimeField(auto_now_add=True)
     endereco = models.CharField(max_length=100)
     produto = models.ForeignKey(Produto, on_delete=models.PROTECT, null=True, blank=True)
+    # produto = models.ManyToManyField(Produto, blank=True)
 
     def __str__(self):
         return f"Pedido {self.id} - R${self.produto.valor }"
