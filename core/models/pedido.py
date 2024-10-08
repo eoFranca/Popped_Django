d3608dfcb32ac82643e7b5290f036727edbb489e
eoFrancaa <erfranca.com@gmail.com>
2024-10-08 15:41:46 -0300

    create manytomany a models pedido

--- core/models/pedido.py
+++ core/models/pedido.py
@@ -7,8 +7,8 @@ class Pedido(models.Model):
 
     data_criacao = models.DateTimeField(auto_now_add=True)
     endereco = models.CharField(max_length=100)
-    produto = models.ForeignKey(Produto, on_delete=models.PROTECT, null=True, blank=True)
-    # produto = models.ManyToManyField(Produto, blank=True)
+    # produto = models.ForeignKey(Produto, on_delete=models.PROTECT, null=True, blank=True)
+    produto = models.ManyToManyField(Produto, blank=True)
 
     def __str__(self):
-        return f"Pedido {self.id} - R${self.produto.valor }"
+        return f"{self.id} - " + str(", ".join(str(produto.nome) + ": R$" + str(produto.valor) for produto in self.produto.all()))
