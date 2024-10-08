817a68926d2738a3218254624726e8dbf9117454
LeandroVish <leandrogustavosantos.info@gmail.com>
2024-10-08 17:32:27 -0300

    .

--- core/models/produto.py
+++ core/models/produto.py
@@ -9,6 +9,7 @@ class Produto(models.Model):
     nome = models.CharField(max_length=100, blank=False, null=True)
     descricao = models.CharField(max_length=100, blank=False, null=True)
     valor = models.DecimalField(max_digits=10, decimal_places=2)
+    quantidade = models.IntegerField(default=0)
     categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, null=True, blank=True)
     capa = models.ForeignKey(
         Image,
