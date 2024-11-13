113f37ac1299c529bf5c932ca4e38ee96440cdc9
LeandroVish <leandrogustavosantos.info@gmail.com>
2024-11-13 15:08:42 -0300

    Migração do Banco de Dados no Supabase

--- core/views/pedido.py
+++ core/views/pedido.py
@@ -6,12 +6,18 @@ from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from core.models import Pedido
-from core.serializers import PedidoSerializer, TotalSerializer
+from core.serializers import PedidoSerializer, TotalSerializer, PedidoCreateSerializer
 
 
 class PedidoViewSet(ModelViewSet):
     queryset = Pedido.objects.all().order_by("id")
-    serializer_class = PedidoSerializer
+
+    def get_serializer_class(self):
+        if self.action in ["list", "retrieve"]:
+            return PedidoSerializer
+        else:
+            return PedidoCreateSerializer
+
 
     @action(detail=False, methods=["get"], url_path="total")
     def total(self, request, *args, **kwargs):
