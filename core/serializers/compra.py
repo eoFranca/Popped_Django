b16e82bd983bcf961651619c7ec9417fa6bd2113
LeandroVish <leandrogustavosantos.info@gmail.com>
2024-12-10 15:23:15 -0300

    correção do Back total

--- core/serializers/compra.py
+++ core/serializers/compra.py
@@ -7,21 +7,22 @@ class ItensCompraSerializer(ModelSerializer):
     total = SerializerMethodField()
 
     def get_total(self, instance):
-        return instance.livro.preco * instance.quantidade
+        return instance.produto.valor * instance.quantidade
 
     class Meta:
         model = ItensCompra
-        fields = ("produto", "quantidade")
+        fields = ("produto", "quantidade", "total")
         depth = 1
 
 
 class CompraSerializer(ModelSerializer):
+    usuario = CharField(source="usuario.email", read_only=True)
+    status = CharField(source="get_status_display", read_only=True)
+    itens = ItensCompraSerializer(many=True, read_only=True)
+
     class Meta:
         model = Compra
-        fields = ("id", "usuario", "status", "itens")
-        usuario = CharField(source="usuario.email", read_only=True)
-        status = CharField(source="get_status_display", read_only=True)
-        itens = ItensCompraSerializer(many=True, read_only=True)
+        fields = ("id", "usuario", "status", "total", "itens")
 
 
 class ItensCompraCreateUpdateSerializer(ModelSerializer):
